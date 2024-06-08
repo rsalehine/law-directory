2ac20f7985c004f2c75b05984a8e23b84ca5aa23
Riadus Salehin <riadmukul@gmail.com>
2024-06-09 00:00:57 +0600

    some public page template updated

--- resources/views/layouts/public/public.blade.php
+++ resources/views/layouts/public/public.blade.php
@@ -13,7 +13,6 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <link href="resources/css/style.css" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
